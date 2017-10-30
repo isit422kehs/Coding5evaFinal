4e0b732e4d24536713f088ac01a40cde11092f37
silvia <123silviayu@gmail.com>
2017-10-29 17:30:15 -0700

    home page

---
+++ ConversionApp/ConversionApp/scripts/main.js
@@ -0,0 +1,19 @@
+﻿
+//adds the menu
+$(document).ready(function () {
+
+    $('div[data-role="header"]').append(
+        '<div data-role="navbar"><ul>' +
+        '<li><a href="index.html">Home</a></li>' +
+        '</ul></div>'
+        );
+
+});
+
+//home
+$(document).on('pagebeforeshow ', '#home-page', function () {  
+    
+    $('p').append('<strong>heya</strong>');
+});
+
+
